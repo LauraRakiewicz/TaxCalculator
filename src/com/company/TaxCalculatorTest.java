d1b74576ea3c853e1bf84a3e945e10591d52d891
laura.rakiewicz <rakiewicz.laura@gmail.com>
2020-01-23 15:52:58 +0100

    formatting and test class

---
+++ src/com/company/TaxCalculatorTest.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaxCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateAdvance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateBasis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void colculatedBasis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateInsurance() {
+    }
+}
